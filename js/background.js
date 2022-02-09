28edecfecf5bdce9840fb5383880e163d1ba0aba
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-02-09 20:56:26 +0900

    Added style

---
+++ js/background.js
@@ -0,0 +1,14 @@
+const images = [ "0.jpg", "1.jpg", "2.jpg", "3.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+function paintImage(){
+    const bgImage = document.createElement("img");  // HTML 요소 생성
+    bgImage.src = `img/${chosenImage}`;
+    bgImage.classList.add("bgImage");
+    // body에 bgImage 추가
+    document.body.appendChild(bgImage);
+}
+
+paintImage();
+
