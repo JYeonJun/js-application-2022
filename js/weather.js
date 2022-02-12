c41597813ff9f215ac12f0b83763d5667b95121d
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-02-12 15:57:44 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,13 @@
-const API_KEY = "7e75015709b39d75b1433ec0f69173b8";
-
 // getCurrentPostion 성공
 function onGeoOk(position){     // position: JavaScript가 제공해주는 user 위치
     const lat = position.coords.latitude;   // 위도
     const lon = position.coords.longitude;   // 경도
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
-        console.log(data.name, data.weather[0].main);
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     }); // promise
 }
 
