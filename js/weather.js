557faf3f7838514106fff715f0b25c6cad58be01
JYeonJun <97449471+JYeonJun@users.noreply.github.com>
2022-02-13 15:29:46 +0900

    Add icon

--- js/weather.js
+++ js/weather.js
@@ -4,9 +4,13 @@ function onGeoOk(position){     // position: JavaScript가 제공해주는 user 
     const lon = position.coords.longitude;   // 경도
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     fetch(url).then(response => response.json()).then(data => {
-        const weather = document.querySelector("#weather span:first-child");
-        const city = document.querySelector("#weather span:last-child");
-        city.innerText = data.name;
+        const city = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather div span");
+        const icon = document.createElement("i");
+        const weather__div = document.querySelector(".weather__temp");
+        icon.setAttribute("class", "fas fa-solid fa-temperature-high");
+        weather.before(icon);
+        city.innerText = `🌏 ${data.name}`;
         weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     }); // promise
 }
